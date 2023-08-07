14a90d044254f0de9cf7d7b794fdafb6dc746463
Jack Belnik <jw.belnik@gmail.com>
2023-08-07 21:20:20 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const matchingBrackets = new Map(bracketsConfig);
+
+  for (const bracket of str) {
+    if (bracket === matchingBrackets.get(stack[stack.length - 1])) {
+      stack.pop();
+    } else {
+      stack.push(bracket);
+    }
+  }
+
+  return stack.length === 0;
+};
